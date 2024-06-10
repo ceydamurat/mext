5ad308883b409e72d6115f48744403a64cf11f6a
ceydamurat <ceyda.murat.44@gmail.com>
2024-06-10 14:34:49 +0300

    sss

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
+import os
+import io
 import streamlit as st
-
 import google.generativeai as genai
+from dotenv import load_dotenv
+from PIL import Image
+from PyPDF2 import PdfReader
+import fitz  # this is pymupdf
+
+
+st.set_page_config(page_title="ATS Sistemi",
+                   page_icon=":robot:",
+                   initial_sidebar_state="expanded")
 
 st.header("Uygulama v1")
 
 genai.configure(api_key= "AIzaSyAmXC386mweCOOW6NgF496s24I1GMNGifQ")
+
+
+@st.cache_resource
+def read_pdf(file):
+    pdfReader = PdfReader(file)
+    count = len(pdfReader.pages)
+
+    all_page_text = ""
+
+    for i in range(count):  # for i in range (0, count-1) ///  for i in range (len(pdfReader.pages))
+        page = pdfReader.pages[i]
+        all_page_text += page.extract_text()
+
+    return all_page_text
+
+
+@st.cache_resource
+def read_pdf_2(file_path):
+    doc = fitz.open(file_path)
+    images = []
+    # count = len(doc)
+    for i in range(len(doc)):  # for i in range(count)
+        page = doc.load_page(i)
+        pix = page.get_pixmap()
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        images.append(img)
+    return images
 
 @st.cache_resource
 def get_gemini_response(prompt):
@@ -29,8 +66,8 @@ def get_gemini_response(prompt):
 
 
     generation_config = {
-            "temperature": 0.9,
-            "top_p": 0.30,
+            "temperature": 0.4,
+            "top_p": 0.95,
             "top_k": 64,
             "max_output_tokens": 18192,
             "response_mime_type": "text/plain",
@@ -42,12 +79,35 @@ def get_gemini_response(prompt):
         generation_config = generation_config,
         model_name ="gemini-1.5-flash-latest")
 
+    prompt_token_count = model.count_tokens(prompt)
+
     response = model.generate_content(prompt).text
 
-    return response
+    response_token_count = model.count_token(response)
 
-prompt = st.text_input("lütfen sorgunuzu beliritniz:")
+    total_token_count = model.count_token(response)
+    total_token_count = int(total_token_count)
 
+    return response, total_token_count
+
+
+
+
+
+st.sidebar.header("ATS v2 Hoşgeldiniz")
+
+deneyim_suresi = st.number_input(label = "Deneyim süresi", min_value=0,max_value = 15, value=2)
+
+
+#st.file
+
+user_input  = st.text_input("lütfen sorgunuzu beliritniz:")
+
+prompt = (f"""
+Sen 10 yıllık tecrübeli bir İk uzmanısın.Eline gelen CV örenğindeki iş ilanı için minimum 5 yıl deneyimli aday aranıyor. Gönderilen CV örneğindeki adayın tecrübe süresi {deneyim_suresi} yıldır.
+         """)
+
+st.sidebar.radio("Döküman uzantınızı seçin", ["PDF", "PDF uzantılı olmayan"])
 
 if st.button("Üret"):
 
